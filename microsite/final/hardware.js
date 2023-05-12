228bc976be34f14756be439cbf5733dd4818a9a4
sharylizareid <59847086+sharylizareid@users.noreply.github.com>
2023-05-12 12:42:47 -0500

    final (unfinished)

---
+++ microsite/final/hardware.js
@@ -0,0 +1,8 @@
+
+let hardSubNavColor = "rgba(48, 68, 98, 1)"
+let hardSubNavColorClear = "rgba(48, 68, 98, 0)"
+
+function subAntenna() {
+    console.log("pressed!")
+    document.getElementById("hard-sub-1").style.backgroundColor = hardSubNavColorClear;
+}
